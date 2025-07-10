172c8a14d46a51917d851fe09c0d1654f1816e68
Zyelixify <49106310+Zyelixify@users.noreply.github.com>
2025-07-10 10:23:41 +0200

    add PWA support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
     '@sidebase/nuxt-auth',
+    '@vite-pwa/nuxt',
   ],
   css: ['~/assets/css/main.css'],
   build: {
@@ -36,6 +37,10 @@ export default defineNuxtConfig({
   devtools: {
     enabled: false,
   },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000
+  },
   ui: {
     theme: {
       colors: [
@@ -47,6 +52,38 @@ export default defineNuxtConfig({
         'warning',
         'error'
       ]
+    }
+  },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'MediGuard',
+      short_name: 'MediGuard',
+      description: 'Medication tracking and reminder application',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa/icon-192x192.svg',
+          sizes: '192x192',
+          type: 'image/svg+xml'
+        },
+        {
+          src: 'pwa/icon-512x512.svg',
+          sizes: '512x512',
+          type: 'image/svg+xml'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
     }
   }
 })
